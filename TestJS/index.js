132c7caa53e8da1faa9e58912d0f8e7f7b629d5c
srushtimane27 <srushtimane31@gmail.com>
2024-01-06 15:47:48 +0530

    test

---
+++ TestJS/index.js
@@ -0,0 +1,19 @@
+// Sum of numbers
+// var num =[1,2,3,4,5]
+
+
+// for(var i=0; i<num.length; i++ ){
+
+//     console.log(num[i])
+  
+    
+// }
+
+//Reverse Array
+
+var arr = [10,20,30,40]
+
+for(var i=0; i<arr.length; i++){
+    console.log(arr[i])
+
+}
